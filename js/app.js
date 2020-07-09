fbedd9dd2b9e87dc947622e963bd33e3640229d1
DJ Stewart <deshastewart1992@gmail.com>
2020-07-08 23:21:58 -0700

    commit the stuff

---
+++ js/app.js
@@ -0,0 +1,50 @@
+function functionWithArguments(argumentOne, argumentTwo) {
+        return argumentOne + argumentTwo   
+    ;}
+   // var answer = functionWithArguments(1,2);
+   // console.log(answer); 
+   console.log(functionWithArguments(2,9)); 
+
+   function functionWithFighting(fightOne, fightTwo, fightThree) {
+    return fightOne + fightTwo + fightThree  
+;}
+console.log(functionWithFighting(5,1,4)); 
+
+function functionWithFisticuffs(knuckleSandwichOne, knuckleSandwichtTwo, knuckleSandwichThree, knuckleSandwichFour) {
+    return knuckleSandwichOne + knuckleSandwichtTwo + knuckleSandwichThree + knuckleSandwichFour  
+;}
+
+console.log(functionWithFisticuffs(3,1,4,1)); 
+
+var dcComics = {
+    "hero": "Superman",
+    "Powers": "Flight",
+    "weakness": "Kryptonite"
+};
+
+console.log(dcComics.hero);
+console.log(dcComics["weakness"]);
+console.log(dcComics.otherPowers = "strength");
+console.log(dcComics.powers= "x ray vision");
+
+var marvelComics = {
+    "hero": "Ironman",
+    "Powers": "Being Lame",
+    "weakness": "Being human"
+};
+
+console.log(marvelComics.Powers);
+console.log(marvelComics["hero"]);
+console.log(marvelComics.films = "too many");
+console.log(marvelComics.weakness = "none");
+
+var videoGames = {
+    "game": "rayman",
+    "rating": "E",
+    "platform": "PC"
+};
+
+console.log(videoGames.game);
+console.log(videoGames["platform"]);
+console.log(videoGames.otherConsoles = "Wii U");
+console.log(videoGames.rating = "the best game ever");
